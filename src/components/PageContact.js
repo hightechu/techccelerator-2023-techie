4e414771411b6c9c6907bab39217abce27697135
Wm2mky <wtk3.co@gmail.com>
2023-08-16 15:30:00 -0700

    new pages

---
+++ src/components/PageContact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PageContact = () => {
+
+  return (
+    <div className="container-fluid">   
+
+       <h1 className="">Contact</h1>
+
+        <p className="">Contact will go here at some point</p>
+
+    </div>
+  );
+
+};
+
+export default PageContact;
---
+++ src/components/PageNearby.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PageNearby = () => {
+
+  return (
+    <div className="container-fluid">   
+
+       <h1 className="">Nearby</h1>
+
+        <p className="">Nearby will go here at some point</p>
+
+    </div>
+  );
+
+};
+
+export default PageNearby;
