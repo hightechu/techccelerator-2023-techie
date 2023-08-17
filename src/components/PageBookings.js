0eab20b794039dc17a1978a4f1150eda77e2a8f6
High Tech U 03 <hightechu2023-03@uvic.ca>
2023-08-17 10:40:22 -0700

    delivery and bookings

--- src/components/PageBookings.js
+++ src/components/PageBookings.js
@@ -5,9 +5,71 @@ const PageBookings = () => {
   return (
     <div className="container-fluid">   
 
-       <h1 className="">Bookings</h1>
+       <h1>Bookings</h1>
 
-        <p className="">Bookings will go here at some point</p>
+  <div className="vstack gap-2 col-md-5 mx-auto">
+
+      <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="Groceries picture" />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Groceries</h5>
+              <p class="card-text">Get your Groceries from your favourite supermarket without leaving your home.</p>
+              <p class="card-text"><small class="text-body-secondary">Some restrictions Apply</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="Takeout picture" />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Takeout</h5>
+              <p class="card-text">Get your next meal from your favourite restaurant without leaving your home.</p>
+              <p class="card-text"><small class="text-body-secondary">Some restrictions Apply.</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="Supplies picture" />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Supplies</h5>
+              <p class="card-text">Get your home items, hygiene products, stationary and more without leaving your home.</p>
+              <p class="card-text"><small class="text-body-secondary">Some restrictions Apply.</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="medication picture" />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Medication</h5>
+              <p class="card-text">Get your prescriptions, on-the-counter medications and more without leaving your home.</p>
+              <p class="card-text"><small class="text-body-secondary">Some restrictions Apply.</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+  </div>
 
     </div>
   );
