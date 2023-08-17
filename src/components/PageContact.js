74feeb3a9f08cd5d02cc0ae4434d6985854f1161
High Tech U 03 <hightechu2023-03@uvic.ca>
2023-08-17 11:16:25 -0700

    cat

--- src/components/PageContact.js
+++ src/components/PageContact.js
@@ -5,9 +5,27 @@ const PageContact = () => {
   return (
     <div className="container-fluid">   
 
-       <h1 className="">Contact</h1>
+       <h1 className="">Contacts</h1>
 
-        <p className="">Contact will go here at some point</p>
+        <div className="vstack gap-2 col-md-5 mx-auto">
+
+          <h2>1. Emergency Number</h2>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                  <div class="col-md-4">
+                    <img src="..." class="img-fluid rounded-start" alt="Blue Bird Cab picture" />
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">911</h5>
+                      <p class="card-text">Fire, police, ambulance and Natural disasters</p>
+                      <p class="card-text"><small class="text-body-secondary">Only for Emergencies. Some restrictions apply</small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+        </div>
 
     </div>
   );
